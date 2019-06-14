424d84f2c8c5ea07bda475987a7d1ea34531813c
IndhurekhaS <42089681+IndhurekhaS@users.noreply.github.com>
2019-06-14 17:38:26 +0530

    Create kth_largest_number.java

---
+++ kth_largest_number.java
@@ -0,0 +1,38 @@
+import java.util.*;
+public class MyClass{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int k=sc.nextInt();
+        int a[]=new int [n];
+        for(int i=0;i<n;i++)
+        {
+            a[i]=sc.nextInt();
+        }
+        int temp=0;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=i+1;j<n;j++)
+            {
+                if(a[i]<a[j])
+                {
+                    temp=a[i];
+                    a[i]=a[j];
+                    a[j]=temp;
+                }
+            }
+            if(k>0)
+            {
+                int l=k-1;
+                if(l==i)
+                {
+                    System.out.print(a[i]+" ");        
+                }
+            }
+            
+        }
+        
+        
+    }
+}
